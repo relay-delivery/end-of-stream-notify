ba5cca8e1175c0b7d47bcacd0e381b66fb357245
Zach Ivester <zivester@gmail.com>
2015-11-16 16:39:06 -0500

    promisify response

--- index.js
+++ index.js
@@ -11,27 +11,26 @@ module.exports = function(stream, opt){
 		};
 	})(opt.label || 'The stream');
 
-	endOfStream(stream, function(err){
-		if (err){
-			console.log(opt.label + ' ended with an error.');
-			console.error(err);
+	return new Promise(function(resolve, reject){
+		endOfStream(stream, function(err){
+			if (err){
+				console.log(opt.label + ' ended with an error.');
+				console.error(err);
+				growl({
+					title : 'bundle error',
+					icon : 'face-angry',
+					message : err.toString()
+				});
+				reject(err);
+			}
 
-			growl({
-				title : 'bundle error',
-				icon : 'face-angry',
-				message : err.toString()
-			});
-
-			if (opt.exitOnError) process.exit(opt.exitOnError);
-
-		} else {
 			console.log(opt.label + ' finished successfully!');
 			growl({
 				message : 'build OK',
 				icon : 'emblem-default',
 				expire : 2000
 			});
-		}
-	
+			resolve();
+		});
 	});
 };
